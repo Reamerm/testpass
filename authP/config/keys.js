3e4db281ced6498474bc4cbf30a5323348a79938
Matthew Reamer <reamer.m.p@gmail.com>
2019-10-08 10:42:06 -0400

    ADDED auth project

---
+++ authP/config/keys.js
@@ -0,0 +1,15 @@
+//add file to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '839255451553-mt4f4eud3vecmson7j96cjlrga560je0.apps.googleusercontent.com',
+    clientSecret: 'oBzZ-FwEv3yEfgNbhbSVpm63'
+
+  },
+  mongodb: {
+    dbURI: 'mongodb+srv://deved:rhino11@cluster0-7lwah.mongodb.net/Dest?retryWrites=true&w=majority'
+  },
+  session: {
+    cookieKey: 'iamthebest'
+  }
+};
